1d61a3ccfa90dcc1d02ae4c95ad4587e7cb7e0f8
trbKnl <ncdeschipper@gmail.com>
2024-10-17 13:00:10 +0200

    footer is gone

--- src/framework/visualisation/react/ui/pages/templates/footer.tsx
+++ src/framework/visualisation/react/ui/pages/templates/footer.tsx
@@ -7,20 +7,6 @@ interface FooterProps {
 export const Footer = ({ left, middle, right }: FooterProps): JSX.Element => {
   return (
     <>
-      <div className='bg-grey4 h-px' />
-      <div className='h-full flex flex-col justify-center'>
-        <div className='flex flex-row gap-4 px-14'>
-          <div className='w-1/3'>
-            {left}
-          </div>
-          <div className='w-1/3'>
-            {middle}
-          </div>
-          <div className='w-1/3'>
-            {right}
-          </div>
-        </div>
-      </div>
     </>
   )
 }
